b682b3d68ae63e4f4b05990e556fecfb33f9b4c2
Damilola Oni <damilolaoni15@gmail.com>
2024-07-08 16:37:16 +0100

    commit

--- index.js
+++ index.js
@@ -8,11 +8,7 @@ const productRoutes = require("./routes/product");
 const app = express();
 
 app.use(express.json());
-app.use(
-  cors({
-    origin: "*",
-  })
-);
+app.use(cors());
 
 app.use((req, res, next) => {
   console.log(req.method, req.path);
