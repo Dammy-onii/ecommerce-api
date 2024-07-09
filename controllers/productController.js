55500ecacf39261530f2ee77efcd862fa5257849
Damilola Oni <damilolaoni15@gmail.com>
2024-07-09 14:47:16 +0100

    201

--- controllers/productController.js
+++ controllers/productController.js
@@ -44,7 +44,7 @@ const createProduct = async (req, res) => {
       stock,
       category,
     });
-    res.status(200).json(product);
+    res.status(201).json(product);
   } catch (error) {
     res.status(400).json({ error: error.message });
     console.log(error);
